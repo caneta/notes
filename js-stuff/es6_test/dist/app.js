608a7a8cbf2bea096a91ce8cf318132fc7d475c8
alessandro.candini <candini@ariadne.it>
2015-07-21 17:52:31 +0200

    Added ES6 examples

--- js-stuff/es6_test/dist/app.js
+++ js-stuff/es6_test/dist/app.js
@@ -1,25 +1,66 @@
-// Fat arrows: Expression bodies
 "use strict";
 
+function printSectionTitle(title) {
+  console.info("\n# -----------------------------\n# " + title + "\n# -----------------------------");
+}
+
+// function printSeparator(){
+// 	console.log(`
+// - - - - - - - - - - - - - - - -`
+// 	)
+// }
+
+printSectionTitle("Fat Arrows");
+
+// Expression bodies
 var evens = [0, 2, 4, 6, 8, 10],
     odds = evens.map(function (v) {
-		return v + 1;
+  return v + 1;
 }),
     nums = evens.map(function (v, i) {
-		return v + i;
+  return v + i;
 }),
     pairs = evens.map(function (v) {
-		return { even: v, odd: v + 1 };
+  return { even: v, odd: v + 1 };
+});
+
+// Statement bodies
+var fives = [];
+nums.forEach(function (v) {
+  if (v % 5 === 0) fives.push(v);
 });
 
 // Multiline strings and string interpolation
-console.log("evens: " + evens + " \nodds: " + odds + "\nnums: " + nums + "\n-------------------");
+console.log("\nevens: " + evens + " \nodds: " + odds + "\nnums: " + nums + "\nfives: " + fives + "\n");
 
-// Fat arrows: Statement bodies
-var fives = [];
-nums.forEach(function (v) {
-		if (v % 5 === 0) fives.push(v);
-});
+// Lexical this
+var bob = {
+  _name: "Bob",
+  _friends: ["Anne", "Tim", "Jack"],
+  printFriends: function printFriends() {
+    var _this = this;
 
-console.log("fives: " + fives);
+    this._friends.forEach(function (f) {
+      return console.log(_this._name + " knows " + f);
+    });
+  }
+};
+
+bob.printFriends();
+
+printSectionTitle("Enhanced Object Literals");
+
+// var obj = {
+//     // __proto__
+//     __proto__: theProtoObj,
+//     // Shorthand for ‘handler: handler’
+//     handler,
+//     // Methods
+//     toString() {
+//      // Super calls
+//      return "d " + super.toString();
+//     },
+//     // Computed (dynamic) property names
+//     [ 'prop_' + (() => 42)() ]: 42
+// };
 //# sourceMappingURL=app.js.map
--- js-stuff/es6_test/src/app.js
+++ js-stuff/es6_test/src/app.js
@@ -1,20 +1,64 @@
-// Fat arrows: Expression bodies
+function printSectionTitle(title){
+	console.info(`
+# -----------------------------
+# ${title}
+# -----------------------------`
+	)
+}
+
+// function printSeparator(){
+// 	console.log(`
+// - - - - - - - - - - - - - - - -`
+// 	)
+// }
+
+printSectionTitle('Fat Arrows')
+
+// Expression bodies
 var evens = [0, 2, 4, 6, 8, 10],
-		odds = evens.map(v => v + 1),
-		nums = evens.map((v, i) => v + i),
+		odds  = evens.map(v => v + 1),
+		nums  = evens.map((v, i) => v + i),
 		pairs = evens.map(v => ({even: v, odd: v + 1}))
 
-// Multiline strings and string interpolation
-console.log(`evens: ${evens} 
-odds: ${odds}
-nums: ${nums}
--------------------`)
-
-// Fat arrows: Statement bodies
+// Statement bodies
 var fives = []
 nums.forEach(v => {
   if (v % 5 === 0)
     fives.push(v);
 });
 
-console.log(`fives: ${fives}`)
+// Multiline strings and string interpolation
+console.log(`
+evens: ${evens} 
+odds: ${odds}
+nums: ${nums}
+fives: ${fives}
+`)
+
+// Lexical this
+var bob = {
+  _name: "Bob",
+  _friends: ["Anne", "Tim", "Jack"],
+  printFriends() {
+    this._friends.forEach(f =>
+      console.log(`${this._name} knows ${f}`))
+  }
+}
+
+bob.printFriends()
+
+printSectionTitle('Enhanced Object Literals')
+
+// var obj = {
+//     // __proto__
+//     __proto__: theProtoObj,
+//     // Shorthand for ‘handler: handler’
+//     handler,
+//     // Methods
+//     toString() {
+//      // Super calls
+//      return "d " + super.toString();
+//     },
+//     // Computed (dynamic) property names
+//     [ 'prop_' + (() => 42)() ]: 42
+// };
